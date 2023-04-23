a89a50bfbdcf9f54ca610af716fa570f1eb425b4
Alexei Rusu <rusualexei1995@gmail.com>
2023-04-23 19:51:09 -0400

    gallery scroll buttons

--- app/(site)/gallery/page.tsx
+++ app/(site)/gallery/page.tsx
@@ -1,30 +1,54 @@
+"use client"
 import { getGallery } from '@/sanity/sanity-utils'
 import Image from 'next/image'
-import React from 'react'
+import { BsChevronCompactLeft, BsChevronCompactRight  } from "react-icons/bs"
+
 
 async function Gallery() {
 
     const gallery = await getGallery()
+
+
+    const slideLeft = () => {
+      const slider =  document.getElementById('slider')
+      slider ? slider.scrollLeft = slider.scrollLeft - 300 : null
+    }
+
+    const slideRight = () => {
+      const slider =  document.getElementById('slider')
+      slider ? slider.scrollLeft = slider.scrollLeft + 300 : null
+    }
     
   return (
-    <div  className='grid grid-cols-1 gap-8 my-8 p-4  xl:my-12 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3'>
-        {gallery?.map((img, i) => (
-            <div key={i}
-            className='w-full my-4 h-64 '
-            >
-                <Image 
-                src={img.image}
-                alt={img.caption}
-                width="300"
-                height="320"
-                className='w-full h-64 object-cover brightness-75 rounded-lg shadow-gray-400 shadow-md hover:-translate-y-2 hover:brightness-100 transition-all'
-                />
-                <p className='italic font-thin w-fit px-4 mt-4 bg-gray-200 rounded-lg '>
-                    {img.caption}</p>
-            </div>
-        ))}
+    <div  className='relative flex items-center align-top'>
+      <BsChevronCompactLeft
+      className='h-[220px] hover:bg-gray-200 rounded-md mx-2'
+      onClick={slideLeft} size={40} />
+      <div id='slider' className='w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth p-4 scrollbar-hide'>
+          {gallery?.map((img, i) => (
+              <div key={i}
+              className='w-fit  p-2 cursor-pointer  inline-block  '
+              >
+                  <Image 
+                  src={img.image}
+                  alt={img.caption && ""}
+                  width="220"
+                  height="220"
+                  className='max-h-[220px] min-w-[220px]object-cover brightness-75 rounded-lg shadow-gray-400 shadow-md hover:-translate-y-2 hover:brightness-100 transition-all'
+                  />
+                  {/* <p className='italic font-thin w-fit px-4 mt-4 bg-gray-200 rounded-lg h'>
+                      {img.caption}</p> */}
+              </div>
+          ))}        
+      </div>
+            <BsChevronCompactRight
+            className='h-[220px] hover:bg-gray-200 rounded-md mx-2'
+            onClick={slideRight} size={40} />
     </div>
   )
 }
 
 export default Gallery
+
+
+{/* <div></div> */}
