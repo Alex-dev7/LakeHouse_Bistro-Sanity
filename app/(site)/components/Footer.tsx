27e7d7d4e38e03c7a7c178e4f1a4dcb71d8a3533
Alexei Rusu <rusualexei1995@gmail.com>
2023-06-10 19:58:41 -0400

    home page fully responsive

--- app/(site)/components/Footer.tsx
+++ app/(site)/components/Footer.tsx
@@ -26,8 +26,8 @@ async function Footer() {
      
   return (
     <>
-    <footer  className="w-full md:h-[400px] flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 gap-4 bg-slate-700  text-white py-4 content-center">
-        <div id="contact" className="flex flex-col gap-2 w-fit sm:mx-auto  md:w-5/6 ">
+    <footer  className="w-full h-fit  flex flex-col md:flex-row flex-wrap lg:grid-cols-3 gap-8 bg-slate-700  text-white  p-4 content-center">
+        <div id="contact" className="flex flex-col gap-2   max-w-[400px] sm:w-[400px] sm:mx-auto  ">
             <h4 className="my-2">CONTACT US</h4>
             <a href={info.reserve} target="_blank" className=" text-bold flex gap-4 hover:text-orange-400">
             <span>&#9734;</span> Make a reservation
@@ -57,11 +57,11 @@ async function Footer() {
             </div>
 
         </div>
-        <div className="sm:mx-auto md:w-4/6 space-y-1">
+        <div className="sm:mx-auto   max-w-[400px] sm:w-[400px] space-y-1">
         <h4 className="my-2">OPENING HOURS</h4>
         {hours?.map((hour, i) => (
             
-            <div key={i} className="flex gap-1 justify-between text-gray-400">
+            <div key={i} className="flex gap-11 justify-between text-gray-400">
                <p>{hour.day}</p>
                <div className="space-x-3">
                    <span>{hour.open}</span>
@@ -72,7 +72,7 @@ async function Footer() {
         ))}
         </div>
         <div className="md:col-span-2 lg:col-auto sm:mx-auto">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d181.10183399873463!2d-73.71274967889809!3d43.42657038721986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89dfc4452d1bb475%3A0xa3c0466e664f3dd7!2sLake%20House%20Bistro!5e0!3m2!1sen!2sus!4v1682192562543!5m2!1sen!2sus" className="w-[400px] h-[250px]  border-[8px] border-zinc-300" allowFullScreen={true} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d181.10183399873463!2d-73.71274967889809!3d43.42657038721986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89dfc4452d1bb475%3A0xa3c0466e664f3dd7!2sLake%20House%20Bistro!5e0!3m2!1sen!2sus!4v1682192562543!5m2!1sen!2sus" className="xs:w-[350px] sm:w-[400px]  h-[250px]  border-[8px] border-zinc-300" allowFullScreen={true} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </footer>
     <div className="flex justify-between px-4 bg-slate-800 text-slate-300 text-sm p-2">
--- app/(site)/components/Nav.tsx
+++ app/(site)/components/Nav.tsx
@@ -6,8 +6,8 @@ import { CiFacebook, CiInstagram } from "react-icons/ci"
 import { TbBrandTiktok } from "react-icons/tb"
 import { getRestaurantInfo } from "@/sanity/sanity-utils"
 import { usePathname } from 'next/navigation'
-import React , { useEffect, useState, useRef} from "react"
-// import SocialLinks from "./SocialLinks"
+import { useEffect, useState} from "react"
+
 
 
 
@@ -15,63 +15,64 @@ function Nav() {
 
 const [toggle, setToggle] = useState(false)
 const [info, setInfo] = useState({})
-const pathname = usePathname();
-
+const pathname = usePathname()
 
 
 
 function handleClick(){
-  
  setToggle(!toggle)
-  
-console.log(toggle)
-
 }
 
-// const arr = []
 
-
-
-  useEffect(() => {
+useEffect(() => {
     const data = async () => {
-      const [...restaurantInfo] = await getRestaurantInfo()
-      setInfo({...restaurantInfo[0]})
-      console.log(info)
+      const [restaurantInfo] = await getRestaurantInfo()
+       return  setInfo( () => restaurantInfo)
+      
     }
     data()
-   
-  }, []);
+}, []);
+
+// useEffect(() => {
+//   if (info) {
+//     console.log( info);
+//   }
+// }, [info]);
 
   return (
-    <div id="div" className="w-full  flex flex-col ">
-        <div className="flex flex-row  justify-between py-5 border-b border-black   px-4">
+    <div id="div"  className="w-full  flex flex-col ">
+        <div className="flex  bg-transparent flex-row z-50  justify-between py-2 md:py-5 border-b border-black relative  px-4">
 
-           <div className="hidden md:flex gap-5 text-lg self-center w-[202px]">
+           <div className="hidden md:flex gap-5 text-lg self-center w-[202px] ">
+            {/* @ts-expect-error Async Server Component */}
               <a href={info?.facebook} target="_blank">
                   <CiFacebook className="text-2xl hover:text-amber-600"/>
               </a>
+              {/* @ts-expect-error Async Server Component */}
               <a href={info?.instagram} target="_blank" >
                   <CiInstagram className="text-2xl hover:text-amber-600"/>
               </a>
+              {/* @ts-expect-error Async Server Component */}
               <a href={info?.tiktok} target="_blank">
                   <TbBrandTiktok className="text-2xl hover:text-amber-600"/>
               </a>                
             </div>
 
             
-            <Link href="/" as={'/'} className="text-xl  md:text-2xl font-bold ">Lake House Bistro</Link>
+            <Link href="/" as={'/'} className="text-xl py-2  md:text-2xl font-bold ">Lake House Bistro</Link>
             <div className="md:hidden" >
-			        <button onClick={() => handleClick()} className=" flex items-center text-gray-600 p-3">
-                  <svg className="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+			        <button onClick={() => handleClick()} className="flex items-center text-gray-600 p-3">
+                  <svg className="block h-6 w-6 fill-current " viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <title>Mobile menu</title>
-                    <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+                    { !toggle ? <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path> :
+                     <path d="M0 0 L20 20 M0 20 L20 0" stroke="white" strokeWidth="2" />}
                   </svg>
 			      </button>
 		      </div>
             <span className="text-sm hidden md:flex text-gray-400 self-center w-[202px] text-right">325 Canada St, Lake George, NY </span>
         </div>
      
-        <nav  className={` ${toggle ? "hidden" : "flex"}  transition-all flex-col md:flex-row  md:visible justify-center  gap-2 text-md antialiased tracking-wide bg-opacity-20 my-1`}>
+        <nav onClick={() => setToggle(false)}  className={` ${toggle ? "static top-0  justify-center text-center text-white z-20 bg-slate-800 bg-opacity-95 w-full h-full " : "absolute text-center w-full h-full -top-[100%]   z-20"} transition-all duration-300 ease-in-out z-0 flex flex-col absolute md:relative md:flex  md:flex-row  md:visible justify-center  gap-2 text-md antialiased tracking-wide bg-opacity-20 my-1 `}>
           <Link href="/" as={'/'} className={`${pathname === "/" ? "bg-amber-100 text-amber-700 font-bold" : ""} link px-4 py-3 hover:text-amber-400 mx-2`} >
                 HOME  
           </Link>  
