882501ecbe1695b0dfe1540328d7115f390bcc3e
Alexei Rusu <rusualexei1995@gmail.com>
2023-06-10 22:49:36 -0400

    bug fix/typescript comment

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -18,12 +18,12 @@ export default function RootLayout({
     <html lang="en"  >
       
       <body className="md:max-w-screen-2xl  mx-auto  box-border">
-        {/* @ts-expect-error Server Component */}
+   
       <Nav />
       <div className=' min-h-screen md:min-h-[700px] lg:min-h-[800px]'>
         {children}
       </div> 
-      {/* @ts-expect-error Server Component */}
+    
         <Footer/>
       </body>
     </html>
